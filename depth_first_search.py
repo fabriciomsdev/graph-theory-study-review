e9b1f1836d22fc31e91a7a43b3715c9663d1eb30
Fabricio Magalhães Sena <fabricioms.dev@gmail.com>
2022-03-03 21:40:49 -0300

    feat: apply preorder and postorder DFS

--- depth_first_search.py
+++ depth_first_search.py
@@ -7,28 +7,44 @@ graph: SimpleGraph = {
     'D': ['K', 'F', 'B', 'C'],
     'F': ['E', 'D'],
     'E': ['F', 'B'],
-    'K': ['C', 'D', 'Z'],
+    'K': ['C', 'Z', 'D',],
     'Z': []
 }
 
 
 class DepthFirstSearchSolver:
-    visited = {}
-    path = []
 
+    def __init__(self):
+        self.marked = {}
+        self.path = []
+    
     def visit(self, node):
-        self.visited[node] = True
         self.path.append(node)
         print(f"Path: {' -> '.join(self.path)}")
 
-    def do_traversal(self, graph: SimpleGraph, current_node):
+    def do_traversal_with_preorder(self, graph: SimpleGraph, current_node):
         neighbors = graph[current_node]
+        self.visit(current_node)
+        self.marked[current_node] = True
         
         for neighborhood in neighbors:
-            if not self.visited.get(neighborhood):
-                self.do_traversal(graph, neighborhood)
+            if not self.marked.get(neighborhood):
+                self.do_traversal_with_preorder(graph, neighborhood)
+        
+        
+    def do_traversal_with_postorder(self, graph: SimpleGraph, current_node):
+        neighbors = graph[current_node]
+        self.marked[current_node] = True
+        
+        for neighborhood in neighbors:
+            if not self.marked.get(neighborhood):
+                self.do_traversal_with_preorder(graph, neighborhood)
                 
         self.visit(current_node)
                 
 
-DepthFirstSearchSolver().do_traversal(graph, 'A')
+print('\n Preorder:')
+DepthFirstSearchSolver().do_traversal_with_preorder(graph, 'A')
+
+print('\n Postorder:')
+DepthFirstSearchSolver().do_traversal_with_postorder(graph, 'B')
